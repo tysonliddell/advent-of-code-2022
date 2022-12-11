4f4bc6cde649699983697b324dbd860c0fa8289e
Tyson Liddell <tysonliddell@hotmail.com>
2022-12-11 08:29:09 +0000

    Day 11 - Part 2.

--- aoc-python/aoc_python/d11/monkey.py
+++ aoc-python/aoc_python/d11/monkey.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass, field
+from functools import reduce
+from operator import mul
 from typing import Callable, List
 
 
@@ -7,15 +9,17 @@ class Monkey:
     num: int
     items: List[int] = field(default_factory=list)
     op: Callable[[int], int] = lambda x: 0
+    divisor: int = 0
     test: Callable[[int], int] = lambda x: 0
     inspection_count: int = 0
 
 
 def do_round(monkeys: List[Monkey]):
+    mod = reduce(mul, [m.divisor for m in monkeys], 1)
     for monkey in monkeys:
         while monkey.items:
             item = monkey.items.pop(0)
             monkey.inspection_count += 1
-            worry_level = monkey.op(item) // 3
+            worry_level = monkey.op(item) % mod
             next_monkey = monkey.test(worry_level)
             monkeys[next_monkey].items.append(worry_level)
